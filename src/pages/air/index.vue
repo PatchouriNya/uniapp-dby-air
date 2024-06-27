5eae242b49dcef7678bc33e7daf51e53b6324597
patchouri <609206398@qq.com>
2024-06-27 15:52:19 +0800

    初始化仓库

---
+++ src/pages/air/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <view class="content">
+    <view class="card" v-for="(airConditioner, index) in airConditioners" :key="index">
+      <image class="icon" :src="airConditioner.icon"></image>
+      <text class="name">{{ airConditioner.name }}</text>
+      <text class="brand">{{ airConditioner.brand }}</text>
+    </view>
+  </view>
+</template>
+
+
+
+<script>
+  export default {
+    data() {
+      return {
+        airConditioners: [
+          {
+            icon: '../../static/ac1.png',
+            name: '空调1',
+            brand: '品牌A'
+          },
+          {
+            icon: '../../static/ac2.png',
+            name: '空调2',
+            brand: '品牌B'
+          },
+          {
+            icon: '../../static/ac3.png',
+            name: '空调3',
+            brand: '品牌C'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          },
+          {
+            icon: '../../static/ac4.png',
+            name: '空调4',
+            brand: '品牌D'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  .content {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 20rpx;
+  }
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    flex-basis: 43%;
+    max-width: 100%;
+    background-color: #fff;
+    margin-bottom: 20rpx;
+    padding: 20rpx;
+    border-radius: 10rpx;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  }
+
+  .icon {
+    width: 100rpx;
+    height: 100rpx;
+    margin-bottom: 10rpx;
+  }
+
+  .name {
+    font-size: 32rpx;
+    font-weight: bold;
+    color: #333;
+    margin-bottom: 5rpx;
+  }
+
+  .brand {
+    font-size: 28rpx;
+    color: #888;
+  }
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <view class="content">
+    <view class="logo-area">
+      <image class="logo" src="/static/logo.png"></image>
+    </view>
+    <view class="text-area">
+      <text class="title">{{title}}</text>
+    </view>
+    <view class="form-area">
+      <input class="input" type="text" placeholder="请输入用户名" />
+      <input class="input" type="password" placeholder="请输入密码" />
+      <button class="login-button" bindtap="onLogin">登录</button>
+      <button class="login-button" @click="gogo">走</button>
+
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const title = ref('欢迎登录')
+const onLogin = () => {
+  console.log('登录成功')
+}
+const gogo = () => {
+  // 跳转到air页面
+  uni.navigateTo({ url: '/pages/air/index' }
+  )
+}
+onLogin()
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: lightskyblue;
+  background-size: cover;
+}
+
+.logo-area {
+  margin-top: -300rpx;
+  margin-bottom: 50rpx;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+}
+
+.text-area {
+  margin-bottom: 50rpx;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #ffffff;
+}
+
+.form-area {
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+}
+
+.input {
+  margin-bottom: 20rpx;
+  padding: 20rpx;
+  border: 1px solid #ddd;
+  border-radius: 10rpx;
+  font-size: 30rpx;
+}
+
+.login-button {
+  margin-top: 30rpx;
+  padding: 20rpx;
+  background-color: #4CAF50;
+  color: #fff;
+  font-size: 36rpx;
+  text-align: center;
+  border-radius: 10rpx;
+}
+</style>
