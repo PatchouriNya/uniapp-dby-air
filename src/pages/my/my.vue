f53ab8fe747c91480ba174bce21f062ec4f6a548
patchouri <609206398@qq.com>
2024-07-01 11:19:59 +0800

    测试pinia持久化存储

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,11 +1,17 @@
 <template>
   <view>
-
+    <view>会员信息:{{memberStore.profile}}</view>
+    <button @tap="memberStore.setProfile({
+    nickname:'123',
+    })" size="mini" plain="true" type="primary">保存用户信息</button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain="true" type="warn">清除用户信息</button>
   </view>
 </template>
 
 <script setup>
-
+import {useMemberStore} from '../../stores'
+const memberStore = useMemberStore()
+console.log(memberStore.profile)
 </script>
 
 
---
+++ src/stores/modules/member.js
@@ -0,0 +1,34 @@
+import {ref} from 'vue'
+import {defineStore} from 'pinia'
+export const useMemberStore = defineStore('member',()=>{
+    // 会员信息
+    const profile = ref()
+
+    // 保存会员信息，登陆时可用
+    const setProfile = (val)=>{
+        profile.value = val
+    }
+    // 清理
+    const clearProfile = ()=>{
+        profile.value = undefined
+    }
+    return{
+
+        profile,
+        setProfile,
+        clearProfile,
+     }
+    },
+    {
+        persist: {
+            storage:{
+                getItem(key){
+                   return uni.getStorageSync(key)
+                },
+                setItem(key, value){
+                    uni.setStorageSync(key, value)
+                }
+            }
+        }
+    },
+)
